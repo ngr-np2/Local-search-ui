f29a19cd0337b21d316d849a414895fede7a2c08
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-03-13 10:31:41 +0000

    fixed image position

--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -4,7 +4,7 @@ import Top from "../../components/home/hero/Top"
 const Hero = () => {
     return (
         <header className=" bg-gray-300">
-            <div className=" z-10 relative mb-4 pb-4 bg-no-repeat  bg-[url('/ITAHARI.jpeg')] bg-bottom bg-cover">
+            <div className=" z-10 relative mb-4 pb-4 bg-no-repeat  bg-[url('/ITAHARI.jpeg')] bg-center bg-cover">
                 <div className="-z-10 h-full backdrop-blur-[1px] absolute w-full bg-[#42444717]" />
                 <Top />
                 <Mid />
