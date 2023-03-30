2aba11fe2453508a34be6c202060dead8ac4d2ca
NGR-NP <ngr112001@gmail.com>
2023-03-30 21:24:43 +0545

    trying lazy loding to components

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,3 +1,5 @@
+
+
 import { createBrowserRouter, Link } from "react-router-dom";
 import GetListedFree from "./pages/GetListedFree";
 import Home from "./pages/Home";
@@ -6,3 +8,19 @@ export const AllRoutes = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/getlistedfree', element: <GetListedFree /> }
 ])
+
+// import { lazy, Suspense } from "react";
+// import { createBrowserRouter, Link } from "react-router-dom";
+// import LoadingCircel from './components/loading/loadingCircle'
+// const Home = lazy(() => import('./pages/Home'))
+// const GetListedFree = lazy(() => import('./pages/GetListedFree'))
+// export const AllRoutes = createBrowserRouter([
+//     { path: '/', element: <Home /> },
+//     {
+//         path: '/getlistedfree',
+//         element:
+//             <Suspense fallback={<LoadingCircle />}>
+//                 <GetListedFree />
+//             </Suspense>
+//     }
+// ])
